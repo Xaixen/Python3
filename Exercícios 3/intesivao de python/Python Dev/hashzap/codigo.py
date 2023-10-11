71e1f635b811f156939e2469a395ccf8ad88d2b5
Marco Antônio Machado Costa <marcoantonio.m.costa01@gmail.com>
2023-10-10 21:00:18 -0300

    flet

--- Exercícios 3/intesivao de python/Python Dev/hashzap/codigo.py
+++ Exercícios 3/intesivao de python/Python Dev/hashzap/codigo.py
@@ -1,24 +1,41 @@
 import flet as ft
 
 def main(pagina):
-    texto = ft.Text('Hashzap')
+    texto = ft.Text('Xablazap', size=20)
     chat = ft.Column()
     nome_usuario = ft.TextField(label='Escreva seu nome')
-    enviar_mensagem = ft.TextField(label='Digite sua mensagem')
-    botao_enviar_mensagem = ft.ElevatedButton('Enviar')
+    
+    def enviar_mensagem_tunel(mensagem):
+        tipo = mensagem['tipo']
+        if tipo == 'mensagem':
+            texto_mensagem = mensagem['texto']
+            usuario_mensagem = mensagem['usuario']
+            chat.controls.append(ft.Text(f"{usuario_mensagem}: {texto_mensagem}")) 
+        else:
+            usuario_mensagem = mensagem['usuario']
+            chat.controls.append(ft.Text(f'{usuario_mensagem} entrou no chat', size=12, italic=True, color=ft.colors.ORANGE_500))
+        pagina.update()
+
+    pagina.pubsub.subscribe(enviar_mensagem_tunel)
+    def enviar_mensagem(evento):
+        pagina.pubsub.send_all({'texto': campo_mensagem.value, 'usuario': nome_usuario.value, 'tipo': 'mensagem'})
+        campo_mensagem.value = ''
+        pagina.update()
+
+    campo_mensagem = ft.TextField(label='Digite sua mensagem')
+    botao_mensagem = ft.ElevatedButton('Enviar', on_click=enviar_mensagem)
     def entrar_popup(evento):
-        pagina.add(chat)
         popup.open = False
+        pagina.pubsub.send_all({'usuario': nome_usuario.value, 'tipo': 'entrada'})
         pagina.remove(botao)
         pagina.remove(texto)
-        pagina.add(enviar_mensagem)
-        pagina.add(botao_enviar_mensagem)
-        pagina.add(ft.Row([enviar_mensagem, botao_enviar_mensagem]))
+        pagina.add(chat)
+        pagina.add(ft.Row([campo_mensagem, botao_mensagem]))
         pagina.update()
     popup = ft.AlertDialog(
         open=False,
         modal=True,
-        title=ft.Text('Bem vindo ao Hashzap'),
+        title=ft.Text('Bem vindo ao Xablazap'),
         content=nome_usuario,
         actions=[ft.ElevatedButton('entrar', on_click=entrar_popup)],
     )
@@ -30,4 +47,4 @@ def main(pagina):
     botao = ft.ElevatedButton('Iniciar chat', on_click=entrar_chat)
     pagina.add(texto)
     pagina.add(botao)
-ft.app(target=main, view=ft.WEB_BROWSER)
+ft.app(target=main, view=ft.WEB_BROWSER, port=8000)
